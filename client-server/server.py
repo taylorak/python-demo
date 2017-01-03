d4019673b995b343e0b03e4cc4dc6f2802cb3247
Taylor Kennedy <taykenned@gmail.com>
2017-01-02 22:36:14 -1000

    started writing a simple socket http server

--- client-server/server.py
+++ client-server/server.py
@@ -2,17 +2,21 @@
 
 import socket
 
-host = socket.gethostname()
-port = 8080;
+#host = socket.gethostname()
+host = ""
+port = 8080
 
 mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mySocket.bind((host,port))
-mySocket.listen()
+mySocket.listen(1)
 
-
+print("starting server")
 while True:
     client, addr = mySocket.accept()
-    message = client.recv(1024).decode();
-    reply = "You asked: " + message
-    client.sendall(reply.encode())
+    #print("request from " + str(addr));
+    request = client.recv(1024).decode()
+    print(request)
+    #reply = "You asked: " + message
+    #client.send(reply.encode())
+    client.send("HTTP/1.0 404 Not Found\r\n".encode())
     client.close()
