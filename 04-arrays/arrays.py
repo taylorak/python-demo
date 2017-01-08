36c9b254026c0ddbc0d58b4906bc4c60f1031cb2
Taylor Kennedy <taykenned@gmail.com>
2017-01-07 21:32:35 -1000

    adds array and array method examples

---
+++ 04-arrays/arrays.py
@@ -0,0 +1,18 @@
+'''
+Working with arrays and array methods
+'''
+
+SHOPPING_LIST = ["apples", "oranges", "bananas"]
+
+print(SHOPPING_LIST)
+
+SHOPPING_LIST.append("grapes")
+
+print(SHOPPING_LIST)
+
+SHOPPING_LIST.pop()
+
+print(SHOPPING_LIST)
+
+SHOPPING_LIST.remove("apples")
+print(SHOPPING_LIST)
